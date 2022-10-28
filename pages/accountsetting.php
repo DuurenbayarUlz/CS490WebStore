c8df1f80848ecd7f1b858349df3e97ad5d8810d5
ariesinattentiv <ariesgirl5959@gmail.com>
2022-10-28 15:33:57 -0400

    moved header/sidebar css to own file, can @import

---
+++ pages/accountsetting.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Settings</title>
+    <link href="accountsettings.css" ref="stylesheet">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous"
+    />
+</head>
+<body>
+    <div id="root">
+    <header>
+            <div class="header-logo"><h2>Webstore</h2></div>
+            <div class="header-input">
+                <input
+                    class="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+            </div>
+            <div class="header-navigation">
+                <div class="header-navigation-account">
+                    <img
+                    src="../images/profile.png"
+                    alt="profile icon"
+                    height="18"
+                    width="18"
+                    />
+                    <p>My account</p>
+                </div>
+                <div class="header-navigation-wishlist">
+                    <img
+                    src="../images/HeartIcon2.png"
+                    alt="heart icon"
+                    height="18"
+                    width="18"
+                    />
+                    <p>Wishlist</p>
+                </div>
+                <div class="header-navigation-cart">
+                    <img
+                    src="../images/shopping-cart.png"
+                    alt="cart icon"
+                    height="18"
+                    width="18"
+                    />
+                    <p>My Cart</p>
+                </div>
+            </div>
+        </header>
+    </div>
+</body>
+</html>
