5f283849098640504b61040349d8570c2801b9be
ariesinattentiv <ariesgirl5959@gmail.com>
2022-11-14 15:15:16 -0500

    bg color, add menu

--- src/pages/product.php
+++ src/pages/product.php
@@ -453,15 +453,16 @@ $conn = null;
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/product-page.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/product-page.css" />
     <title>Product</title>
 </head>
 
 <body>
     <?php include("partials/header.php") ?>
+    <?php include("partials/menu.php")?>
     <?php echo (!empty($messageProductExisted)) ? $messageProductExisted : ''; ?>
 
     <?php
@@ -469,7 +470,7 @@ $conn = null;
     echo (!empty($messageVoteCasted)) ?  $messageVoteCasted : '';
     echo (!empty($messageRateFirst)) ?  $messageRateFirst : '';
     ?>
-
+<main style="background-color:rgb(253, 240, 213);">
     <div class="product-container">
         <div class="product-section">
             <div class="product-section-image">
@@ -638,6 +639,7 @@ $conn = null;
             </div>
         </div>
     </div>
+</main>
     <?php include("partials/footer.php") ?>
 </body>
 
